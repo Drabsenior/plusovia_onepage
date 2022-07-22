fe98ef59c792528bddc80043b89237ffd0eeb512
Drabsenior <drasenior@gmail.com>
2022-07-22 11:32:32 +0300

    slider

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,39 +4,38 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Gallery.css";
 const Gallery = () => {
-  const setting = {};
   let settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false,
     responsive: [
       {
         breakpoint: 900,
-        setting: {
+        settings: {
           dots: true,
           slidesToShow: 1,
         },
       },
       {
         breakpoint: 1150,
-        setting: {
-          slidesToShow: 3,
+        settings: {
+          slidesToShow: 2,
         },
       },
       {
         breakpoint: 1300,
-        setting: {
-          slidesToShow: 4,
+        settings: {
+          slidesToShow: 3,
         },
       },
     ],
   };
   return (
     <div className="gallerycontainer">
-      <Slider {...settings} {...setting}>
+      <Slider {...settings}>
         <div className="galleryimg">
           <img
             src="https://cdn.pixabay.com/photo/2018/05/18/15/30/web-design-3411373__340.jpg"
